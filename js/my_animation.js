2001bacbde8a693487e65855c46862d1aa0c7149
Yassin Kisrawi <yassinkisrawi42@gmail.com>
2019-04-01 15:16:45 +0100

    including react

--- js/my_animation.js
+++ js/my_animation.js
@@ -1,22 +1,3 @@
-// // Scroll to specific values
-// // scrollTo is the same
-// window.scroll({
-//     top: 0,
-//     left: 0,
-//     behavior: 'smooth'
-// });
-
-// // Scroll certain amounts from current position 
-// window.scrollBy({
-//     top: 100, // could be negative value
-//     left: 0,
-//     behavior: 'smooth'
-// });
-
-// // Scroll to a certain element
-// document.querySelector('.hello').scrollIntoView({
-//     behavior: 'smooth'
-// });
 if ($('#back-to-top').length) {
     var scrollTrigger = 100, // px
         backToTop = function () {
