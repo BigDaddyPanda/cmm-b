ccbd0849e94c647ac25a00791449951bf6321585
Draegar_home <yassinkisrawi42@gmail.com>
2019-05-04 18:40:54 +0100

    uploading chaining algo

--- _backend/main.py
+++ _backend/main.py
@@ -1,21 +1,63 @@
-from flask import Flask,render_template
+import os
+from flask import Flask, request, redirect, url_for, send_from_directory, jsonify
+from werkzeug.utils import secure_filename
+from flask_cors import CORS, cross_origin
+from uuid import uuid4
+
 from flask_pymongo import PyMongo
 
-app = Flask(__name__,template_folder='static')
+app = Flask(__name__, template_folder="static")
 app.config["MONGO_URI"] = "mongodb://localhost:27017/myDatabase"
 mongo = PyMongo(app)
+UPLOAD_FOLDER = r"D:\DSEN-2\cmm-b\_backend\_img"
+ALLOWED_EXTENSIONS = set(["txt", "pdf", "png", "jpg", "jpeg", "gif"])
+
+app = Flask(__name__)
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+CORS(app)
 
 
-@app.route("/")
-def home_page():
-    online_users = mongo.db.users.find({"online": True})
-    return render_template("index.html",
-        online_users=online_users)
+def allowed_file(filename):
+    return "." in filename and filename.rsplit(".", 1)[1].lower() in ALLOWED_EXTENSIONS
 
-@app.route("/user/<username>")
-def user_profile(username):
-    user = mongo.db.users.find_one_or_404({"_id": username})
-    return render_template("user.html",user=user)
+
+@app.route("/downloader", methods=["GET", "POST"])
+def download(filename="###"):
+    if filename == "###":
+        filename = request.args.get("filename")
+    return send_from_directory(directory=UPLOAD_FOLDER, filename=filename)
+
+
+@app.route("/uploader", methods=["GET", "POST"])
+def upload_file():
+    print("ez", request.files)
+    if request.method == "POST":
+        # check if the post request has the file part
+        if "file" not in request.files:
+            print("No file part")
+            return redirect(request.url)
+        file = request.files["file"]
+        # if user does not select file, browser also
+        # submit a empty part without filename
+        if file.filename == "":
+            print("No selected file")
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = str(uuid4()) + "." + file.filename.rsplit(".", 1)[1].lower()
+            # filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config["UPLOAD_FOLDER"], filename))
+            # return redirect(url_for("upload_file", filename=filename))
+            return jsonify(url=f"http://127.0.0.1:5000/downloader?filename={filename}")
+    return """
+    <!doctype html>
+    <title>Upload new File</title>
+    <h1>Upload new File</h1>
+    <form method=post enctype=multipart/form-data>
+      <p><input type=file name=file>
+         <input type=submit value=Upload>
+    </form>
+    """
+
 
 if __name__ == "__main__":
     app.run(debug=True)
